4ece33d96d7c961969c986232e87a9846c302c7e
kyle <75395106+ehre9305@users.noreply.github.com>
2021-11-24 15:02:18 -0600

    make limelight utility class

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -4,32 +4,36 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-// import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.Limelight;
+import frc.robot.Limelight.Pipeline;
 
 /** An example command that uses an example subsystem. */
 public class Drive extends CommandBase {
   @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
   private final Drivetrain m_subsystem;
-  // private Timer timer;
-  private NetworkTable limeTable;
-  private NetworkTableEntry tx;
-  private NetworkTableEntry ty;
-  private NetworkTableEntry ta;
-  private NetworkTableEntry led;
+  private Limelight lime = Limelight.getInstance();
+  private Limelight.TargetDistancer targetDistancer = lime.new TargetDistancer(9.5, 32.1, 52);
+
+  private NetworkTableEntry log = NetworkTableInstance.getDefault().getTable("SmartDashboard").getSubTable("DB")
+      .getEntry("String 0");
+  private NetworkTableEntry log2 = NetworkTableInstance.getDefault().getTable("SmartDashboard").getSubTable("DB")
+      .getEntry("String 1");
+  private NetworkTableEntry log3 = NetworkTableInstance.getDefault().getTable("SmartDashboard").getSubTable("DB")
+      .getEntry("String 2");
+  // private NetworkTableEntry log4 = NetworkTableInstance.getDefault().getTable("SmartDashboard").getSubTable("DB").getEntry("String 3");
+
+  private NetworkTableEntry aimDistance = NetworkTableInstance.getDefault().getTable("SmartDashboard").getSubTable("DB")
+      .getEntry("Slider 0");
 
   private static final int MAXHORIZDEGREES = 27;
-  private static final double MAXVERTDEGREES = 20.5;
   private static final double TURNSPEED = 0.5;
   private static final double TURNBASE = 0.3;
-  private static final double MOVESPEED = 0.5;
+  private static final double MOVESPEED = 1.0;
   private static final double MOVEBASE = 0.2;
-  // private static final double MOVESPEED = 0;
-  // private static final double MOVEBASE = 0;
 
   /**
    * Creates a new Drive.
@@ -45,46 +49,36 @@ public class Drive extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    limeTable = NetworkTableInstance.getDefault().getTable("limelight");
-    tx = limeTable.getEntry("tx");
-    ty = limeTable.getEntry("ty");
-    ta = limeTable.getEntry("ta");
-    led = limeTable.getEntry("ledMode");
-    led.setNumber(0);
+    lime.leds.auto();
+
+    aimDistance.setDouble(60);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // if (timer.get() < 1)
-    //   m_subsystem.arcadeDrive(0.5, 0);
-    // else if (timer.get() < 2)
-    //   m_subsystem.tankDrive(0, 0.5);
-    // else if (timer.get() < 3)
-    //   m_subsystem.tankDrive(0.5, 0);
-    // m_subsystem.arcadeDrive(0, Math.signum(limeTable.getEntry("tx").getDouble(0)));
-
     //read values
-    double x = tx.getDouble(0.0);
-    double y = ty.getDouble(0.0);
-    double area = ta.getDouble(0.0);
+    double x = lime.getX();
 
-    double limeHeight = 10;
-    double limeAngle = 33;
-    double targetHeight = 43;
+    double goalDistance = aimDistance.getDouble(60);
+    double currentDistance = targetDistancer.get();
+    double distanceOffset = goalDistance - currentDistance;
 
-    System.out.println((targetHeight - limeHeight) / Math.tan(Math.toRadians(limeAngle + y)));
-    // System.out.println("Y: " + y);
+    log.setString("" + distanceOffset);
+    log2.setString("" + currentDistance);
 
-    m_subsystem.arcadeDrive(-y / MAXVERTDEGREES * MOVESPEED - Math.signum(y) * MOVEBASE,
-        x / MAXHORIZDEGREES * TURNSPEED + Math.signum(x) * TURNBASE);
+    double speed = -Math.signum(distanceOffset) * (1 - 1 / (1 + Math.abs(distanceOffset) / 13));
+    log3.setString("" + speed);
+
+    m_subsystem.arcadeDrive(speed * MOVESPEED,
+        Math.abs(x) < 3 ? 0 : x / MAXHORIZDEGREES * TURNSPEED + Math.signum(x) * TURNBASE);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     m_subsystem.stop();
-    led.setNumber(1);
+    lime.leds.disable();
   }
 
   // Returns true when the command should end.
